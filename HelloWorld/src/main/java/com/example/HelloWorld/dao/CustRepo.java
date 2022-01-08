466d51fea9fd08702cca3336b7af7d00fff014b4
Naimish95 <96973656+Naimish95@users.noreply.github.com>
2022-01-08 19:04:08 +0530

    Update CustRepo.java

--- HelloWorld/src/main/java/com/example/HelloWorld/dao/CustRepo.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/dao/CustRepo.java
@@ -1,10 +1,18 @@
 package com.example.HelloWorld.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
 import com.example.HelloWorld.model.CustForDeal;
 
 public interface CustRepo extends JpaRepository<CustForDeal,Integer>{
 
+	@Query("delete from cust_for_deal where deal_id=?1")
+	public void deleteByDealId(int dealId);
+	
+	@Query("from cust_for_deal where deal_id=?1 and cust_id=?2")
+	public Optional<CustForDeal> findCustByDealId(int dealId,int custId);
 }
