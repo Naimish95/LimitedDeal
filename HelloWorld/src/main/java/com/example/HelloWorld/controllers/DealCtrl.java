0f3eff3853e2d1f3472695facff27fe35f4c852a
Naimish95 <96973656+Naimish95@users.noreply.github.com>
2022-01-08 19:01:23 +0530

    Update DealCtrl.java

--- HelloWorld/src/main/java/com/example/HelloWorld/controllers/DealCtrl.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/controllers/DealCtrl.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.chrono.ChronoLocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -29,44 +30,31 @@ import com.example.HelloWorld.model.Deal;
 public class DealCtrl {
 
 	@Autowired
-	DealRepo dr;
+	DealRepo dealRepo;
 	
 	@Autowired
-	CustRepo cr;
+	CustRepo custRepo;
 	
 	@PostMapping("/createDeal")
 	public String createDeal(Deal d){
-		dr.save(d);
+		dealRepo.save(d);
 		return "Deal with id="+d.getDealId()+" created successfully";
 	}
 	@DeleteMapping("/endDeal/{did}")
 	public String endDeal(@PathVariable("did")int dealId) throws SQLException{
-		Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/test","root","root");
-		PreparedStatement ps=con.prepareStatement("Select * from Deal where dealId=?1");
-		ps.setInt(1, dealId);
 		
-		ResultSet rs=ps.executeQuery();  
-		while(rs.next()){  
-		int maxitem=rs.getInt("maxItems");
-		int ItemsBought=rs.getInt("ItemsBought");
-		Timestamp curT=Timestamp.valueOf(LocalDateTime.now());
-		Timestamp maxTime=rs.getTimestamp("maxtime");
-			if(ItemsBought>=maxitem){
-				dr.deleteById(dealId);
-				PreparedStatement ps1=con.prepareStatement("Delete from CustForDeal where dealId=?1");
-				ps1.setInt(1, dealId);
-				ps1.executeUpdate();
-				return "Deal with id="+dealId+ " deleted as items are sold out";
-			}
-			if(curT.after(maxTime)){
-				dr.deleteById(dealId);
-				PreparedStatement ps1=con.prepareStatement("Delete from CustForDeal where dealId=?1");
-				ps1.setInt(1, dealId);
-				ps1.executeUpdate();
-				return "Deal with id="+dealId+ " deleted as time is over";
-			}
+		Deal d=dealRepo.findById(dealId).get();
+		if(d.getItemsBought()>=d.getMaxItems()){
+			dealRepo.deleteById(dealId);
+			custRepo.deleteByDealId(dealId);
+			return "Deal with id="+dealId+" is ended as maximum limit has reached.";
+		}
+		if(LocalDateTime.now().isAfter(d.getMaxtime())){
+			dealRepo.deleteById(dealId);
+			custRepo.deleteByDealId(dealId);
+			return "Deal with id="+dealId+" is ended as time limit is over.";
+		}
 		
-		}  
 		return "Deal cant be ended";
 	}
 	@PutMapping("/updateDeal/{did}")
@@ -76,41 +64,30 @@ public class DealCtrl {
 		Deal dprev=dr.getById(dealId);
 		dprev.setMaxItems(maxItems);
 		dprev.setMaxtime(maxTime);
-		dr.save(dprev);
+		dealRepo.save(dprev);
 		return "Deal has been updated";
 	}
 	@PostMapping("/claimDeal/{did}")
 	public String claimDeal(@PathVariable("did")int dealId,CustForDeal c) throws SQLException{
-		//check for items bought
-		Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/test","root","root");
 		//check if person has bought deal before
-		PreparedStatement ps1=con.prepareStatement("Select * from CustForDeal where dealId=?1 and custId=?2");
-		ps1.setInt(1, dealId);
-		ps1.setInt(2, c.getCustId());
-		ResultSet rs1=ps1.executeQuery();
-		if(rs1==null){
-			//chcek if items bought<maxItems
-			PreparedStatement ps=con.prepareStatement("Select * from Deal where dealId=?1");
-			ps.setInt(1, dealId);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next()){
-				int maxitem=rs.getInt("maxItems");
-				int ItemsBought=rs.getInt("ItemsBought");
-				Timestamp now=Timestamp.valueOf(LocalDateTime.now());
-				Timestamp maxTime=rs.getTimestamp("maxtime");
-				if(ItemsBought<maxitem && now.before(maxTime)){
-					PreparedStatement ps2=con.prepareStatement("Update Deal set ItemsBought=?1 where dealId=?2");
-					ps2.setInt(1, ItemsBought+1);
-					ps2.setInt(2, dealId);
-					ps2.executeUpdate();
-					cr.save(c);
-				}
-				else return "deal cant be made as time is over or maxlimit has reached";
-			}
-			
-		}
-		return "You have already bought this item";
+		Optional<CustForDeal> cust=custRepo.findCustByDealId(dealId, c.getCustId());
 		
+		if(!cust.isPresent()){
+			//chcek if items bought<maxItems
+			Deal d=dealRepo.findById(dealId).get();
+			if(d.getItemsBought()<d.getMaxItems() && LocalDateTime.now().isAfter(d.getMaxtime())){
+			d.setItemsBought(d.getItemsBought()+1);
+			dealRepo.save(d);
+			custRepo.save(c);
+			}
+			else return "Deal cant be made as time is over or maxlimit has reached";
+		}
+		else{
+			return "You have already bought this item";
+		
+		}
+		return null;
 		
 	}
+	
 }
